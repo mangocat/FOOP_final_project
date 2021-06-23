576e1ae2d641be96bed80b6161e8f0f75e473607
mangocat <lilin.boardgame@gmail.com>
2021-06-23 16:32:49 +0800

    yicheng's code is back.

---
+++ src/controller/Game.java
@@ -0,0 +1,27 @@
+package controller;
+
+import team.Human; /*may modify here*/
+import model.World;
+
+public class Game extends GameLoop {
+	private final Human humanUnit;
+	private final World world;
+
+	public Game(World world, Human humanUnit){
+		this.humanUnit = humanUnit;
+		this.world = world;
+	}
+
+	public void humanLevelUp(){
+		this.humanUnit.levelUp();
+	}
+
+	public void humanSummon(String spriteName){
+		this.humanUnit.createSprite(spriteName);
+	}
+
+	@Override
+	protected World getWorld() {
+		return this.world;
+	}
+}
