00ea229779d40ea75f465d414f42d0c1e47c3884
mangocat <lilin.boardgame@gmail.com>
2021-06-23 12:20:00 +0800

    change to non-atomic moveState so that once the range is enough, the unit will try to attack

--- src/MoveState.java
+++ src/MoveState.java
@@ -29,18 +29,28 @@ public class MoveState extends State{
             unit.setState(dead);
             return;
         }
+        Direction face = unit.getFace();
+        Rectangle range = unit.getRange();
+        int enemyBattleLine = unit.getEnemyBattleLine();
+        int attackDistance = unit.getAttackDistance();
+        int front = unit.getFront();
+        State next;
+        if((face == Direction.LEFT && enemyBattleLine+attackDistance >= front) || (face == Direction.Right && front+attackDistance >= enemyBattleLine)){
+            reset();
+            // if the range is enough, try to stop and attack
+            if(unit.getCurrentAttackCd() == 0){
+                next = unit.getState("attack");
+            }else{
+                next = unit.getState("idle");
+            }
+            next.update();
+            unit.setState(next);
+            return;
+        }
         currentPosition++;
         if(currentPosition >= images.size()){
             reset();
-            // need enemyTeam battleLine
             // if can move: move
-            Direction face = unit.getFace();
-            Rectangle range = unit.getRange();
-            int enemyBattleLine = unit.getEnemyBattleLine();
-            int attackDistance = unit.getAttackDistance();
-            int front = unit.getFront();
-            State next;
-            // int moveDistance;
             if((face == Direction.LEFT && enemyBattleLine+attackDistance < front) || (face == Direction.Right && front+attackDistance < enemyBattleLine)){
                 next = unit.getState("move");
             }else if(unit.getCurrentAttackCd() == 0){// else if can attack: attack
