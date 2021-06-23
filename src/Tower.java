e4f0a16ae61d76ddc093021067c40c06eb59f3d0
B06902016 <b06902016@ntu.edu.tw>
2021-06-23 10:57:10 +0800

    tower added

--- src/Tower.java
+++ src/Tower.java
@@ -1,33 +1,34 @@
 import java.awt.*;
 import java.lang.Math;
+import java.util.Collection;
 
 public class Tower extends Sprite{
     private int hp;
     private int attackDistance;
-    private int damage;
+    public static final int damage = 10;
     private Point location;
+    public static final int maxAttackCd = 500;
+    private int currentAttackCd;
 
     public Tower(int hp, int attackDistance, Point location){
         this.hp = hp;
         this.attackDistance = attackDistance;
         this.location = location;
+        this.currentAttackCd = 0;
     }
 
     @Override
     public void update() {
-
+        currentAttackCd -= 1;
+        if(currentAttackCd <= 0){
+            Collection<Sprite> attackableUnit = team.getWorld().getSprites(face, location, attackDistance);
+            for(Sprite sprite : attackableUnit){
+                sprite.takeDamage(damage);
+            }
+            currentAttackCd = maxAttackCd; // set cd to max value
+        }
     }
 
     @Override
-    public void render(Graphics g) {
-
-    }
-
-    public void attack(Team opponentTeam) {
-        for(Unit opponent : opponentTeam){
-            if(Math.abs(opponent.getLocation().x - this.location.x) <= attackDistance){
-                opponent.takeDamage(damage);
-            }
-        }
-    }
+    public void render(Graphics g) {}
 }
